57de987958d857cff3b90342ef16fe9ea9243db5
LHofstee <laurahfst@gmail.com>
2021-11-03 11:48:01 +0100

    Create duplicate subset
    This part of the function creates a subset of the duplicates in the dataset.
    
    The missing doi's need to be taken out, otherwise they will count as duplicates. The subset is needed to determine the sets of duplicates.

--- scripts/deduplicate.R
+++ scripts/deduplicate.R
@@ -2,5 +2,9 @@ deduplicate <- function(df){
   # Remove irrelevant duplicates
   df <- filter(df, final_included == 1 | unique_record == 1 | is.na(unique_record))
   
+  # Create a subset of the duplicates
+  df_doi <- filter(df, !is.na(doi))
+  df_dup <- get_dupes(df_doi, doi)
+  
   return(df)
 }
