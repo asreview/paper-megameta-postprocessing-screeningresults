03eaebdf1116e82cccea0e310a441f77c0015e37
Brand <s.a.g.e.vandenbrand@uu.nl>
2021-12-03 11:31:29 +0100

    Add information on missing titles, abstracts and doi's

--- scripts/print_information_datasets.R
+++ scripts/print_information_datasets.R
@@ -6,8 +6,15 @@ included_excluded <- function (df,name, included_column){
   cat(paste("The", name, "dataset contains", nrow(df), "records.\n",
             "Of which there are: \n", 
             nrow(df %>% filter(included_column == 1)), "included records. \n",
-            nrow(df %>% filter(included_column == 0)), "excluded records."
+            nrow(df %>% filter(included_column == 0)), "excluded records.",
+            "\n \n",
+            "This dataset also contains:\n",
+            length(which(!is.na(df$title))), "/", nrow(df), "titles. \n",
+            length(which(!is.na(df$abstract))), "/", nrow(df), "abstracts. \n",
+            length(which(!is.na(df$doi))), "/", nrow(df), "doi's. \n"
   ))
+  
+  
   
   
 }
